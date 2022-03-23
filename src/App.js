39bfb65929ec451c64eec3d0574e97b14293752e
shashifw11 <shashi_fw11_220@masai.school>
2022-03-23 21:49:05 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from "./components/Navbar/button"
+function App() {
+   
+  return (
+    <div className="App">
+    {/* <h1>shashi</h1> */}
+    <Button/>
+     
+    
+
+
+    </div>
+  );
+}
+
+export default App;
